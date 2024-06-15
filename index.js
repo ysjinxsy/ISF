6a02a34830b218f2f57d5e1262d99e8f354b4948
ysjinxsy <168466901+ysjinxsy@users.noreply.github.com>
2024-06-15 19:32:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.listen(port, () => {
   console.log(`🔗 Powered By RTX`);
 });
 
-const statusMessages = ["Playing 𝗣𝗹𝗮𝘆𝗶𝗻𝗴 𝗶𝗻 𝗜𝗦𝗙 𝗟𝗲𝗮𝗴𝘂𝗲 ⚽"];
+const statusMessages = ["Working 𝗪𝗼𝗿𝗸𝗶𝗻𝗴 𝗜𝗻 𝗪𝗼𝗻𝗱𝗲𝗿."];
 
 let currentIndex = 0;
 const channelId = '';
